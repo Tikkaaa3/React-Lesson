979888e98a4bcebc2201a9db882d83aac17157de
Tikkaaa3 <tikkaaa3@gmail.com>
2023-12-06 23:04:06 +0100

    ep4

---
+++ ep4/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+    const [age, setAge] = useState(0);
+    const [inputValue, setInputValue] = useState('');
+    const increaseAge = () => {
+        setAge(age + 1)
+    };
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value)
+    };
+    const [showText, setShowText] = useState(true);
+    const [changeColor, setChangeColor] = useState('black');
+  return (
+    <div className="App">
+        
+      {age}
+      <button type='button' onClick={increaseAge}>Increase Age</button>
+      
+      <input type='text' onChange={handleInputChange}/>
+      {inputValue}
+
+      <button type='button' onClick={() => {setChangeColor(changeColor === 'black' ? 'red' : 'black')}}>change color</button>
+      <button type='button' onClick={() => {setShowText(!showText)}}>show/hide</button>
+      {showText && <h1 style={{color: changeColor}}>HELLO I AM TIKKA</h1>}
+
+    </div>
+  );
+}
+
+export default App;
