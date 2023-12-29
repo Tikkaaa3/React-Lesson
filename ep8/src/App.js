b5a24dfb685437ce517bb5a56f1fe34322c75d54
Tikkaaa3 <tikkaaa3@gmail.com>
2023-12-29 16:28:39 +0100

    fetch

--- ep8/src/App.js
+++ ep8/src/App.js
@@ -5,24 +5,27 @@ import { Profile } from "./pages/Profile";
 import { Contact } from "./pages/Contact";
 import { Navbar } from "./pages/navbar";
 import { useState, createContext } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const AppContext = createContext()
 
 function App() {
   const [username, setUsername] = useState('')
-
+  const client = new QueryClient()
   return (
     <AppContext.Provider value={{ username, setUsername }}>
       <div className="App">
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
-          </Routes>
-        </Router>
+        <QueryClientProvider client={client}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+            </Routes>
+          </Router>
+        </QueryClientProvider>
       </div>
     </AppContext.Provider>
   );
